cd366841c98f33576274a6662f9f3e05b7fb24e7
Aleef123 <aleefnaushad12@gmail.com>
2023-09-18 09:55:02 +0530

    added delete end point

---
+++ src/main/java/com/example/h2databasedemomaven/Repository/StudentDao.java
@@ -0,0 +1,34 @@
+package com.example.h2databasedemomaven.Repository;
+
+import com.example.h2databasedemomaven.domain.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Slf4j
+public class StudentDao {
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    public static final String SELECT_STUDENT="select NAME1,AGE,EMAIL from " +
+            "STUDENT where ID=:id";
+    public Student getStudentDetails(String id){
+        MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource();
+        sqlParameterSource.addValue("id",id);
+        log.info(String.valueOf(id));
+        return namedParameterJdbcTemplate.query(SELECT_STUDENT,sqlParameterSource,rs->{
+            Student student=new Student();
+            student.setId(id);
+            student.setName(rs.getString("NAME1"));
+            student.setAge(rs.getInt("AGE"));
+            student.setEmail(rs.getString("EMAIL"));
+            return student;
+        });
+    }
+}
---
+++ src/main/java/com/example/h2databasedemomaven/Repository/StudentDataDeleteDao.java
@@ -0,0 +1,20 @@
+package com.example.h2databasedemomaven.Repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDataDeleteDao {
+    public static final String STUDENT_DATA_DELETE="delete from STUDENT where ID=:id";
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public void deleteData(String id){
+        MapSqlParameterSource inputparam = new MapSqlParameterSource();
+        inputparam.addValue("id",id);
+        namedParameterJdbcTemplate.update(STUDENT_DATA_DELETE,inputparam);
+    }
+}
